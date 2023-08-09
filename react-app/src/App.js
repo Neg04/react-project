87298480733cc3ae1c0ecf4641cbe7dda99802df
neg04 <neg.n04@gmail.com>
2023-08-09 16:14:33 +0330

    added weather.js code

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import Weather from "./Weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Weather />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Neg04/react-project.git"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        Source Code
         </a>
-      </header>
     </div>
   );
 }
